4a8d78c198b422dbbfef39d84102806fb6163e73
brandmorgan <brandmorgan@gmail.com>
2023-04-11 19:17:31 -0700

    wired js, re-worked input fields

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
 //Business Logic for Pizza-Parlor//
-
-
-function Pizza(small) {
-    this.toppings = [];
-    this.size = small;
-    this.cost = 0;
-}
-console.log(Pizza);
+function calculateTotal() {
+    // Get the size and base price
+    const sizeInputs = document.getElementsByName("size");
+    let sizePrice = 0;
+    for (const sizeInput of sizeInputs) {
+      if (sizeInput.checked) {
+        sizePrice = parseInt(sizeInput.value);
+        break;
+      }
+    }
+    
+    // Get the selected veggies and calculate the veggie price
+    const veggieInputs = document.getElementsByName("veggies");
+    let veggiePrice = 0;
+    let numVeggies = 0;
+    for (const veggieInput of veggieInputs) {
+      if (veggieInput.checked) {
+        veggiePrice += parseInt(veggieInput.value);
+        numVeggies++;
+        if (numVeggies === 3) {
+          break;
+        }
+      }
+    }
+    
+    // Get the selected meats and calculate the meat price
+    const meatInputs = document.getElementsByName("meats");
+    let meatPrice = 0;
+    let numMeats = 0;
+    for (const meatInput of meatInputs) {
+      if (meatInput.checked) {
+        meatPrice += parseInt(meatInput.value);
+        numMeats++;
+        if (numMeats === 3) {
+          break;
+        }
+      }
+    }
+    
+    // Calculate the total price
+    const totalPrice = sizePrice + veggiePrice + meatPrice;
+    
+    // Display the total price to the user
+    const totalPriceElement = document.getElementById("totalPrice");
+    totalPriceElement.textContent = `Total price: $${totalPrice}`;
+  }
